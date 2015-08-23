1f6e00e6a77c81b576667f68b7334bbb93190510
Anastasiya Zaslavskaya <zaslavskayasya@gmail.com>
2015-08-24 02:36:58 +0300

    5 screens

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,6 +5,17 @@ $(document).ready(function(){
  
   pause: 2000
 });
-
-
+	$('#slider2').bxSlider({
+  mode: 'fade',
+  auto: true,
+ 
+  pause: 2000,
+  responsive: true
+});
+$('#slider3').bxSlider({
+  mode: 'fade',
+  auto: true,
+ 
+  pause: 2000
+});
 }); // - > ready_end;
